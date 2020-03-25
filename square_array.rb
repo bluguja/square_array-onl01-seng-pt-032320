8427dc9d62b66c9627cc1de2dedd3a0783ddb456
Bala Luguja <bluguja@gmail.com>
2020-03-25 19:39:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+
+numbers = [1,2,3]
+new_numbers = [9,10,16,25]
+
 def square_array(array)
-  # your code here
+  
+  new_array []
+  
+  array.each do |number| 
+      
+    # statement to be executed 
+    print number **2
+  end
+      
 end
+    
+print square_array(numbers)
+print square_array(new_numbers)
