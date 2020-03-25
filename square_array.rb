045a446cd5def9f5c7af7037036f98aff013892b
Bala Luguja <bluguja@gmail.com>
2020-03-25 19:52:17 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,17 +2,9 @@
 #numbers = [1,2,3]
 #new_numbers = [9,10,16,25]
 
+d
 def square_array(array)
-  
-  #new_array [] << square_array
-  
-  array.each do |number| 
-      
-    # statement to be executed 
-    print number **2
-  end
-      
+  arr = []
+  array.each { |number| arr << number ** 2 }
+  arr
 end
-    
-  square_array(numbers)
-  square_array(new_numbers)
